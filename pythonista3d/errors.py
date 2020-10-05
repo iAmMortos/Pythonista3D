124c3e83544f2a0576709eafb8c9d44036e188ec
Taylor Lopez <taylor0lopez@gmail.com>
2020-10-05 00:41:19 -0600

    Defined first of vector methods, still need others.

--- pythonista3d/errors.py
+++ pythonista3d/errors.py
@@ -1,14 +1,19 @@
 
 class MatrixIndexOutOfBoundsError(Exception):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+  def __init__(self, *args):
+    super().__init__(*args)
 
 
 class MatrixSizeMismatchError(Exception):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+  def __init__(self, *args):
+    super().__init__(*args)
 
 
 class MatrixHasNoInverseError(Exception):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
+  def __init__(self, *args):
+    super().__init__(*args)
+
+
+class VectorSizeMismatchError(Exception):
+  def __init__(self, *args):
+    super().__init__(*args)
--- pythonista3d/vectors.py
+++ pythonista3d/vectors.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import math
+from pythonista3d.errors import VectorSizeMismatchError
 from pythonista3d.points import Point
 from numbers import Number
 
@@ -9,25 +11,38 @@ class Vector(Point):
     super().__init__(ndims, *vals)
 
   def add(self, vec: Vector) -> Vector:
-    pass
+    if self.num_dimensions != vec.num_dimensions:
+      raise VectorSizeMismatchError("Vertices must be the same dimensions to add. [%s] and [%s] provided." %
+                                    (self.num_dimensions, vec.num_dimensions))
+    return Vector(self.num_dimensions, *[self[i] + vec[i] for i in range(self.num_dimensions)])
 
   def subtract(self, vec: Vector) -> Vector:
-    pass
+    if self.num_dimensions != vec.num_dimensions:
+      raise VectorSizeMismatchError("Vertices must be the same dimensions to subtract. [%s] and [%s] provided." %
+                                    (self.num_dimensions, vec.num_dimensions))
+    return Vector(self.num_dimensions, *[self[i] - vec[i] for i in range(self.num_dimensions)])
 
   def multiply(self, vec: Vector) -> Vector:
-    pass
+    if self.num_dimensions != vec.num_dimensions:
+      raise VectorSizeMismatchError("Vertices must be the same dimensions to multiply. [%s] and [%s] provided." %
+                                    (self.num_dimensions, vec.num_dimensions))
+    return Vector(self.num_dimensions, *[self[i] * vec[i] for i in range(self.num_dimensions)])
 
   def add_const(self, const: Number) -> Vector:
-    pass
+    return Vector(self.num_dimensions, *[self[i] + const for i in range(self.num_dimensions)])
 
   def subtract_const(self, const: Number) -> Vector:
-    pass
+    return Vector(self.num_dimensions, *[self[i] - const for i in range(self.num_dimensions)])
 
   def scale(self, scalar: Number) -> Vector:
-    pass
+    return Vector(self.num_dimensions, *[self[i] * scalar for i in range(self.num_dimensions)])
 
   def normalize(self) -> Vector:
-    pass
+    m = self.get_magnitude()
+    return Vector(self.num_dimensions, *[n / m for n in self._vals])
+
+  def get_magnitude(self):
+    return math.sqrt(sum([n ** 2 for n in self._vals]))
 
 
 class Vector2D(Vector):
