c1a34a639828db94ffe45ecc89a0caf0101f06d9
Taylor Lopez <taylor0lopez@gmail.com>
2020-10-06 17:29:57 -0600

    Making nice demo of transformation builder in test.py

--- test.py
+++ test.py
@@ -5,16 +5,27 @@ from pythonista3d.points import Point2D
 from pythonista3d.transform import Transform2D, Transform2DBuilder, ReflectionLine2D
 
 def main():
+  print("Step-by-step transformation:")
   pt = Point2D(3, 1)
+  print(pt)
   pt = Transform2D.shear(pt, 3, -2)
+  print(pt)
   pt = Transform2D.translate(pt, -3, 10)
+  print(pt)
   pt = Transform2D.scale(pt, 3, 3)
+  print(pt)
   pt = Transform2D.rotate(pt, math.pi/2)
+  print(pt)
   pt = Transform2D.reflect(pt, ReflectionLine2D.origin)
   print(pt)
 
+  print("\nCombined transformation:")
   builder = Transform2DBuilder()
-  builder.shear(3, -2).translate(-3, 10).scale(3, 3).rotate(math.pi / 2).reflect(ReflectionLine2D.origin)
+  builder.shear(3, -2)\
+         .translate(-3, 10)\
+         .scale(3, 3)\
+         .rotate(math.pi / 2)\
+         .reflect(ReflectionLine2D.origin)
   pt2 = Point2D(3, 1)
   print(builder.apply(pt2))
 
