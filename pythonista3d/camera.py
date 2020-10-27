01acee4e73dace93efeb26de21c196664422fcb3
Taylor Lopez <taylor0lopez@gmail.com>
2020-10-27 00:54:54 -0600

    Added the Python 3.6 virtual environment for consistency between machines. Added stub modules for STL and COLLADA files. Added several example 3d model files of varying types.

--- pythonista3d/camera.py
+++ pythonista3d/camera.py
@@ -1,3 +1,25 @@
 
 class Camera(object):
-  pass
+  def __init__(self, pos, look_dir, up_dir, n_dist, f_dist, fov):
+    self._pos = pos
+    self._look_dir = look_dir
+    self._up_dir = up_dir
+    self._near_plane_dist = n_dist
+    self._far_plane_dist = f_dist
+    self._field_of_view = fov
+
+  @property
+  def pos(self):
+    return self._pos
+
+  @pos.setter
+  def pos(self, pos):
+    self._pos = pos
+
+  @property
+  def look_dir(self):
+    return self._look_dir
+
+  @look_dir.setter
+  def look_dir(self, look_dir):
+    self._look_dir = look_dir
