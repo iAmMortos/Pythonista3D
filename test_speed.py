fd174c9aabc6d8c3a7b854c4f122a39b4d4cd437
Taylor Lopez <taylor.d.lopez@lmco.com>
2020-10-26 22:41:07 -0600

    Created a test to show the benefits of using primitives rather than thousands of objects. Result was about 20-50x faster using primitives and arithmetic in my matrix multiplication implementation.

--- test_speed.py
+++ test_speed.py
@@ -6,18 +6,43 @@ from pythonista3d.transform3d import Transform3DBuilder, RotationAxis
 from datetime import datetime
 
 
-def main():
-  pts = []
-  tb = Transform3DBuilder().rotate(RotationAxis.y, math.pi / 4)
+def test_with_objects(pts, tb):
   tstart = datetime.now().timestamp()
-  print("Generating random 3D points.")
+  transformed = [tb.apply(p) for p in pts]
+  print("Done transforming all points: %s" % (datetime.now().timestamp() - tstart))
+  print(transformed[:10])
+
+
+def test_with_primitives(pts, tb):
+  ml = tb.build().as_list()
+  newlist = []
+  for pt in pts:
+    newlist.append(pt.x)
+    newlist.append(pt.y)
+    newlist.append(pt.z)
+
+  def mult(m, p1, p2, p3):
+    return (m[0] * p1 + m[1] * p2 + m[2] * p3 + m[3],
+            m[4] * p1 + m[5] * p2 + m[6] * p3 + m[7],
+            m[8] * p1 + m[9] * p2 + m[10] * p3 + m[11])
+
+  transformedlist = []
+  tstart = datetime.now().timestamp()
+  for i in range(0, len(newlist), 3):
+    transformedlist.append(mult(ml, newlist[i], newlist[i+1], newlist[i+2]))
+  print("Done transforming all points: %s" % (datetime.now().timestamp() - tstart))
+  print(transformedlist[:10])
+
+
+
+def main():
+  tb = Transform3DBuilder().rotate(RotationAxis.y, math.pi / 4)
+  pts = []
   for _ in range(100000):
     pts.append(Point3D(random.random(), random.random(), random.random()))
-  print("Done creating points: %s" % (datetime.now().timestamp() - tstart))
 
-  tstart = datetime.now().timestamp()
-  [tb.apply(p) for p in pts]
-  print("Done transforming all points: %s" % (datetime.now().timestamp() - tstart))
+  test_with_objects(pts, tb)
+  test_with_primitives(pts, tb)
 
 
 if __name__ == '__main__':
