30205fb621c5a2d42d5ae89b0c5856d672dda044
Taylor Lopez <taylor0lopez@gmail.com>
2020-10-27 18:07:54 -0600

    Starting documentation for stl_file module.

--- pythonista3d/fileformats/stl_file.py
+++ pythonista3d/fileformats/stl_file.py
@@ -4,16 +4,30 @@ from pythonista3d.points import Point3D
 
 
 class STLMode(Enum):
+  """
+  The different types of STL files.
+
+  - ascii: human-readable, ascii data
+  - binary: binary data
+  """
   ascii = 0
   binary = 1
 
 
 class STLFacet(object):
+  """
+  A single facet as defined by an STL file. Contains the three points in 3D space that make up this face, as well as the
+  vector normal to this face.
+  """
   def __init__(self, normalstr: str):
     self.normal = Point3D(*[float(n) for n in normalstr.split()[-3:]])
     self.vs = []
 
-  def add_vertex(self, vstr):
+  def add_vertex(self, vstr: str):
+    """
+    Parse the given vertex string line from the STL file as a 3D Point, then add it to our short list of vertices.
+    :param vstr: The string vertex line from the STL file to add to this facet definition
+    """
     self.vs.append(Point3D(*[float(n) for n in vstr.split()[-3:]]))
 
   def __repr__(self):
@@ -21,8 +35,10 @@ class STLFacet(object):
 
 
 class STLFile(object):
+  """
+  Represents an STL file and its data. Handles the file operations needed to read and interact with an STL file.
+  """
   def __init__(self, path: str):
-    print("Opening file: [%s]" % path)
     self._path = path
     self._mode = None
     self._cur_line = 1
@@ -32,6 +48,9 @@ class STLFile(object):
     self.load()
 
   def load(self):
+    """
+    Load the STL file that exists at the path specified during creation of this object and parse its data.
+    """
     if not os.path.exists(self._path):
       raise Exception("File not found: [%s]" % self._path)
     with open(self._path) as f:
@@ -54,5 +73,8 @@ class STLFile(object):
         self._cur_facet.add_vertex(line)
       elif line.startswith("endloop"):
         self._facets.append(self._cur_facet)
-        print(self._cur_facet)
         self._cur_facet = None
+
+  def print_facets(self):
+    for facet in self._facets:
+      print(facet)
